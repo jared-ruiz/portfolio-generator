d04ffebfaec8214c7f7dfbe6721acd534716ab31
Jared Ruiz <jared707@gmail.com>
2022-03-29 21:52:22 -0500

    Added inquirer module.

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 //allows us to use the fs module (file system)
 const fs = require("fs");
+const inquirer = require("inquirer");
 
 //module for creating page template
 const generatePage = require("./src/page-template.js");
 
-//takes command line inputs and cuts off after the 1 index to only capture user input
-const profileDateArgs = process.argv.slice(2);
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
-//assigns commandline input to variables [0][1]
-const [name, github] = profileDateArgs;
+// //utilize page-template.js
+// const pageHTML = generatePage(name, github);
 
-//file name that will be created/output file, data that is being written, callback that will handle errors + success message
-fs.writeFile("./index.html", generatePage(name, github), err => {
-    if (err) throw new Error(err);
+// //file name that will be created/output file, data that is being written, callback that will handle errors + success message
+// fs.writeFile("./index.html", pageHTML, err => {
+//     if (err) throw err;
 
-    console.log("Portfolio complete! Check out index.html to see the output!");
-});
+//     console.log("Portfolio complete! Check out index.html to see the output!");
+// });
