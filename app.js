5b1dffa8b7abb831a0f359701f757fecd7f012ce
Jared Ruiz <jared707@gmail.com>
2022-04-01 03:42:23 -0500

    Added all the code to get the html and object data working properly.

--- app.js
+++ app.js
@@ -140,7 +140,12 @@ Add a New Project
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('./index.html', pageHTML, err => {
+          if (err) throw new Error(err);
+
+          console.log('Page created! Check out index.html in this directory to see it!');
     })
 
 // //utilize page-template.js
@@ -151,4 +156,4 @@ promptUser()
 //     if (err) throw err;
 
 //     console.log("Portfolio complete! Check out index.html to see the output!");
-// });
+});
